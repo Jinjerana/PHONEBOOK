cf8c3b30b6f3ae95c70d3698c6662159e1ce64f8
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-10-23 12:48:59 +0200

    14

--- src/Redux/Contacts/contactsAPI.js
+++ src/Redux/Contacts/contactsAPI.js
@@ -3,17 +3,37 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/contacts');
-      return response.data;
+      const { data } = await axios.get('/contacts');
+      token.set(data.token);
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
+
+// const register = createAsyncThunk('auth/register', async credentials => {
+//   try {
+//     const { data } = await axios.post('/users/signup', credentials);
+//     token.set(data.token);
+//     return data;
+//   } catch (error) {
+//     Report.warning('You enter invalid Input. Try again.');
+//   }
+// });
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',

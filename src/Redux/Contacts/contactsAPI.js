a61d64c72b871cda0c8a591230018fda425cb7df
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-10-23 13:20:52 +0200

    15

--- src/Redux/Contacts/contactsAPI.js
+++ src/Redux/Contacts/contactsAPI.js
@@ -1,46 +1,27 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
-
-const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  },
-};
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get('/contacts');
-      token.set(data.token);
-      return data;
+      const response = await axios.get('/contacts');
+      return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-// const register = createAsyncThunk('auth/register', async credentials => {
-//   try {
-//     const { data } = await axios.post('/users/signup', credentials);
-//     token.set(data.token);
-//     return data;
-//   } catch (error) {
-//     Report.warning('You enter invalid Input. Try again.');
-//   }
-// });
-
 export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.post('/contacts', newContact);
-      return response.data;
+      const { data } = await axios.post('/contacts', newContact);
+
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -49,10 +30,10 @@ export const addContact = createAsyncThunk(
 
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
-  async (id, thunkAPI) => {
+  async (contactId, thunkAPI) => {
     try {
-      const { data } = await axios.delete(`/contacts/${id}`);
-      return data.id;
+      const { data } = await axios.delete(`/contacts/${contactId}`);
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
--- src/Redux/Contacts/contactsSlice.js
+++ src/Redux/Contacts/contactsSlice.js
@@ -18,6 +18,7 @@ const contactsSlice = createSlice({
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.isLoading = false;
         state.items = action.payload;
+        state.error = null;
       })
       .addCase(fetchContacts.rejected, (state, action) => {
         state.isLoading = false;

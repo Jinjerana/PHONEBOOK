67cb74825667c75ed44e29405a061dfd24b3d221
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-10-24 12:16:46 +0000

    Deploying to gh-pages from @ Jinjerana/goit-react-hw-08-phonebook@7f84e158262dd252a4d4777a746e69fcc2e99832 🚀

---
+++ static/js/561.4af3bae3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[561],{6561:function(e,t,o){o.r(t);var l=o(184),r={hello:{width:200,marginLeft:15,color:"#002266"}};t.default=function(){return(0,l.jsx)("p",{style:r.hello,children:"Hallo there! It's time to start compiling your professional phonebook. Register and let`s go!"})}}}]);
+//# sourceMappingURL=561.4af3bae3.chunk.js.map

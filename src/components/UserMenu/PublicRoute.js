9cfd6a5d70cc10bd1d63fee4eab2b636dadf52c5
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-10-25 12:11:12 +0200

    isLoggedIn

--- src/components/UserMenu/PublicRoute.js
+++ src/components/UserMenu/PublicRoute.js
@@ -3,7 +3,7 @@ import authSelectors from '../../Redux/auth/auth-selectors';
 import { Navigate } from 'react-router-dom';
 
 export const PublicRoute = ({ component: Component, redirectTo = '/' }) => {
-  const { isLoggedIn } = useSelector(authSelectors.getIsLoggedIn);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
 };
